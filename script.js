6a303724c88d19285108049f491f5e950afe0331
brock cameron <114433184+bthecreat0r@users.noreply.github.com>
2024-03-22 08:56:43 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const todoList=['make breakfast', 'wash dishes'];
+
+let todoListHTML="";
+
+for(let i = 0; i < todoList.length; i++){
+    const todo = todoList[i];
+    const html = `<p> ${todo} </p>`
+    todoListHTML += html;
+}
+console.log(todoListHTML);
+
+function addTodo(){
+    const inputElement = document.querySelector('.js-name-input')
+    const name = inputElement.Value;
+
+    // console.log(name);
+    todoList.push(name);
+    console.log(todoList);
+}
